f94c5e3728ce8c34b40b358da8027e21beb176ef
Darshan Shah <dshah003@terpmail.umd.edu>
2020-04-12 23:36:07 -0400

    Added move and rotate functions

--- arduino_code/marky_controller/marky_controller.ino
+++ arduino_code/marky_controller/marky_controller.ino
@@ -14,8 +14,9 @@ const int left_wheel_pin = 2;
 const int right_wheel_pin = 3;
 const int time_of_velocity = 100;
 
-const double a = 0.0051;
+const double a = 0.0051; // Derived these constants from practical experimentation data
 const double b = 0.416;
+const float L = 0.13;
 std_msgs::Int8 linear_vel;
 
 Servo LeftWheel;
@@ -29,14 +30,24 @@ void MobilityCallback(const geometry_msgs::Twist& vel_msg) {
   double lin_vel = vel_msg.linear.x;
   double ang_vel = vel_msg.angular.z;
   int lin_pwm = 160;
-  if(lin_vel < 0.2){
-    lin_pwm = (lin_vel + b) / a;
-  }
-  linear_vel.data = lin_pwm;
+  // Tested:
+  // if(lin_vel < 0.2){
+  //   lin_pwm = (lin_vel + b) / a;
+  // }
+  // linear_vel.data = lin_pwm;
+  // vel_pub.publish(&linear_vel);
+  // LeftWheel.write(lin_pwm);
+  // RightWheel.write(166-lin_pwm);
+  // delay(time_of_velocity);
+
+  const int gain = 395;
+  float left_wheel_value = gain*(lin_vel + (ang_vel*L)) + 81.5;
+  float right_wheel_value = gain*(lin_vel - (ang_vel*L)) + 81.5;
+  linear_vel.data = int(left_wheel_value);
   vel_pub.publish(&linear_vel);
-  LeftWheel.write(lin_pwm);
-  RightWheel.write(166-lin_pwm);
-  delay(time_of_velocity);
+  LeftWheel.write(left_wheel_value);
+  RightWheel.write(166- right_wheel_value);
+  // delay(time_of_velocity);
   return;  
 }
 
@@ -45,17 +56,21 @@ ros::Subscriber<geometry_msgs::Twist> velocity_sub("/mark_four/cmd_vel", Mobilit
 void setup() {
   LeftWheel.attach(left_wheel_pin);
   RightWheel.attach(right_wheel_pin);
+  
   LeftWheel.write(83);
   RightWheel.write(83);
+  
   nh.initNode();
+
   nh.subscribe(velocity_sub);
   nh.advertise(vel_pub);
+  
   nh.loginfo("Arduino Ready");
 }
 
 void loop() {  
-    LeftWheel.write(83);
-    RightWheel.write(83);
+    // LeftWheel.write(83);
+    // RightWheel.write(83);
     nh.spinOnce();
     delay(1);    
 }
--- src/controller/controller.cpp
+++ src/controller/controller.cpp
@@ -3,48 +3,150 @@
 #include <geometry_msgs/Twist.h>
 #include <sensor_msgs/Joy.h>
 
-
+const double PI = 3.14159265359;
 
 class MarkFourController
 {
   public:
-  MarkFourController();
-  void JoyCallback(const sensor_msgs::Joy::ConstPtr& joy);
-  void TeleOpM4();
+    MarkFourController();
+    void JoyCallback(const sensor_msgs::Joy::ConstPtr& joy);
+    void move(double speed, double distance, bool isForward);
+    void rotate (double angular_speed, double angle, bool clockwise);
+    double degrees2radians(double angle_in_degrees);
+    // void setDesiredOrientation (double desired_angle_radians);
+    // void poseCallback(const turtlesim::Pose::ConstPtr & pose_message);
+    // void moveGoal(turtlesim::Pose  goal_pose, double distance_tolerance);
 
   private:
-  ros::NodeHandle nh_;
-  int linear_, angular_;
-  double l_scale_, a_scale_;
-  ros::Publisher vel_pub_;
-  ros::Subscriber joy_sub_;
-
+    ros::NodeHandle nh_;
+    int linear_, angular_;
+    double l_scale_, a_scale_;
+    ros::Publisher vel_pub_;
+    ros::Subscriber joy_sub_;
 };
 
-MarkFourController::MarkFourController():
-  linear_(1),
-  angular_(1)
-  {
-    nh_.param("axis_linear", linear_, linear_);
-    nh_.param("axis_angular", angular_, angular_);
-    nh_.param("scale_angular", a_scale_, a_scale_);
-    nh_.param("scale_linear", l_scale_, l_scale_);
-    vel_pub_ = nh_.advertise<geometry_msgs::Twist>("mark_four/cmd_vel", 1);
-    joy_sub_ = nh_.subscribe<sensor_msgs::Joy>("joy", 10, &MarkFourController::JoyCallback, this);
-  }
 
-  void MarkFourController::JoyCallback(const sensor_msgs::Joy::ConstPtr& joy) {
-    geometry_msgs::Twist twist;
-    twist.angular.z = a_scale_*joy->axes[angular_];
-    twist.linear.x = l_scale_*joy->axes[linear_];
-    vel_pub_.publish(twist);
-  }
+MarkFourController::MarkFourController() {
+  linear_ = 1;
+  angular_ = 3;
+  nh_.param("axis_linear", linear_, linear_);
+  nh_.param("axis_angular", angular_, angular_);
+  nh_.param("scale_angular", a_scale_, a_scale_);
+  nh_.param("scale_linear", l_scale_, l_scale_);
+  vel_pub_ = nh_.advertise<geometry_msgs::Twist>("mark_four/cmd_vel", 1);
+  joy_sub_ = nh_.subscribe<sensor_msgs::Joy>("joy", 10, &MarkFourController::JoyCallback, this);
+}
 
 
-int main(int argc, char** argv)
-{
-  ros::init(argc, argv, "teleop_turtle");
+void MarkFourController::JoyCallback(const sensor_msgs::Joy::ConstPtr& joy) {
+  geometry_msgs::Twist twist;
+  twist.angular.z = a_scale_*joy->axes[angular_];
+  twist.linear.x = l_scale_*joy->axes[linear_];
+  vel_pub_.publish(twist);
+}
+
+
+void MarkFourController::move(double speed, double distance, bool isForward) {
+  geometry_msgs::Twist vel_msg;
+  //set a random linear velocity in the x-axis
+  if (isForward)
+    vel_msg.linear.x =-abs(speed);
+  else
+    vel_msg.linear.x =abs(speed);
+  vel_msg.linear.y =0;
+  vel_msg.linear.z =0;
+
+  //set a random angular velocity in the y-axis
+  vel_msg.angular.x = 0;
+  vel_msg.angular.y = 0;
+  vel_msg.angular.z = 0;
+
+  double t0 = ros::Time::now().toSec();
+  double current_distance = 0.0;
+  
+  ros::Rate loop_rate(50);
+  do{
+    vel_pub_.publish(vel_msg);
+    double t1 = ros::Time::now().toSec();
+    current_distance = speed * (t1-t0);
+    ros::spinOnce();
+    loop_rate.sleep();
+    //std::cout<<(t1-t0)<<", "<<current_distance <<", "<<distance<<endl;
+  } while(current_distance<distance);
+  vel_msg.linear.x = 0;
+  vel_pub_.publish(vel_msg);
+  ROS_INFO("Move Executed");
+  return;
+}
+
+void MarkFourController::rotate (double angular_speed, double relative_angle, bool clockwise) {
+  geometry_msgs::Twist vel_msg;
+  //set a random linear velocity in the x-axis
+  vel_msg.linear.x =0;
+  vel_msg.linear.y =0;
+  vel_msg.linear.z =0;
+  //set a random angular velocity in the y-axis
+  vel_msg.angular.x = 0;
+  vel_msg.angular.y = 0;
+
+  if (clockwise)
+    vel_msg.angular.z = -abs(angular_speed);
+  else
+    vel_msg.angular.z = abs(angular_speed);
+
+  double current_angle = 0.0;
+
+  double t0 = ros::Time::now().toSec();
+  ros::Rate loop_rate(10);
+  do{
+    vel_pub_.publish(vel_msg);
+    double t1 = ros::Time::now().toSec();
+    current_angle = angular_speed * (t1-t0);
+    ros::spinOnce();
+    loop_rate.sleep();
+  }while(current_angle<relative_angle);
+
+  vel_msg.angular.z =0;
+  vel_pub_.publish(vel_msg);
+  ROS_INFO("Done Rotating");
+  return;
+}
+
+double degrees2radians(double angle_in_degrees) {
+  return angle_in_degrees *PI /180.0;
+}
+
+
+int main(int argc, char** argv) {
+  ros::init(argc, argv, "mark_four");
   MarkFourController markfour_controller;
+
+  double speed, angular_speed;
+  double distance, angle;
+  bool isForward, clockwise;
+  ROS_INFO("\n \n *** TESTING *** \n \n ");
+  // while(true) {
+    // std::cout<<"Enter speed: ";
+    // std::cin>>speed;
+    // std::cout<<"Enter distance: ";
+    // std::cin>>distance;
+    // std::cout<<"forward?: ";
+    // std::cin>>isForward;
+    // markfour_controller.move(speed, distance, isForward);
+    // ros::Duration(3).sleep();
+    // ROS_INFO("\n\n*MOVED FORWARD*\n\n");
+
+    std::cout<<"enter angular velocity (degree/sec): ";
+    std::cin>>angular_speed;
+    std::cout<<"enter desired angle (degrees): ";
+    std::cin>>angle;
+    std::cout<<"clockwise ?: ";
+    std::cin>>clockwise;
+    markfour_controller.rotate(degrees2radians(angular_speed), 
+        degrees2radians(angle), clockwise);
+    ros::Duration(3).sleep();
+    ROS_INFO("\n\n*DONE TURNING*\n\n");
+      
   ros::spin();
 }
 
